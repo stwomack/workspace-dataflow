680e794b3773dfedc9c68b54fbe34f82f59ace22
Steve Womack <stwomack@gmail.com>
2016-06-21 15:42:31 -0500

    Fixes conf files

--- local-configs.sh
+++ local-configs.sh
@@ -1,9 +1,10 @@
-cf set-env s-c-dataflow-admin CLOUDFOUNDRY_API_ENDPOINT https://api.local.pcfdev.io
-cf set-env s-c-dataflow-admin CLOUDFOUNDRY_ORGANIZATION pcfdev-org
-cf set-env s-c-dataflow-admin CLOUDFOUNDRY_SPACE pcfdev-space
-cf set-env s-c-dataflow-admin CLOUDFOUNDRY_DOMAIN local.pcfdev.io
-cf set-env s-c-dataflow-admin CLOUDFOUNDRY_SERVICES my-redis, my-rabbitmq
-cf set-env s-c-dataflow-admin SECURITY_OAUTH2_CLIENT_USERNAME admin
-cf set-env s-c-dataflow-admin SECURITY_OAUTH2_CLIENT_PASSWORD admin
-cf set-env s-c-dataflow-admin SECURITY_OAUTH2_CLIENT_ACCESS_TOKEN_URI https://api.local.pcfdev.io/oauth/token
-cf set-env s-c-dataflow-admin SECURITY_OAUTH2_CLIENT_USER_AUTHORIZATION_URI https://api.local.pcfdev.io/oauth/authorize
+cf set-env dataflow-server CLOUDFOUNDRY_API_ENDPOINT=https://api.local.pcfdev.io
+cf set-env dataflow-server CLOUDFOUNDRY_ORGANIZATION=pcfdev-org
+cf set-env dataflow-server CLOUDFOUNDRY_SPACE=pcfdev-space
+cf set-env dataflow-server CLOUDFOUNDRY_DOMAIN=local.pcfdev.io
+cf set-env dataflow-server CLOUDFOUNDRY_SERVICES=my-redis,my-rabbitmq
+cf set-env dataflow-server SECURITY_OAUTH2_CLIENT_USERNAME=admin
+cf set-env dataflow-server SECURITY_OAUTH2_CLIENT_PASSWORD=admin
+cf set-env dataflow-server SECURITY_OAUTH2_CLIENT_ACCESS_TOKEN_URI=https://api.local.pcfdev.io/oauth/token
+cf set-env dataflow-server SECURITY_OAUTH2_CLIENT_USER_AUTHORIZATION_URI=https://api.local.pcfdev.io/oauth/authorize
+cf set-env dataflow-server SPRING_CLOUD_DEPLOYER_CLOUDFOUNDRY_BUILDPACK=java_buildpack_offline
