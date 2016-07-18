4e4d3edc1d499f664e1994d7c2271932f8d6784c
Steve Womack <stwomack@gmail.com>
2016-07-17 22:04:21 -0500

    Updates

--- pcf1-configs.sh
+++ pcf1-configs.sh
@@ -2,7 +2,7 @@ cf set-env womack-dataflow-server spring.cloud.deployer.cloudfoundry.url https:/
 cf set-env womack-dataflow-server spring.cloud.deployer.cloudfoundry.org swomack-org
 cf set-env womack-dataflow-server spring.cloud.deployer.cloudfoundry.space development 
 cf set-env womack-dataflow-server spring.cloud.deployer.cloudfoundry.domain pcf1.fe.gopivotal.com
-cf set-env womack-dataflow-server spring.cloud.deployer.cloudfoundry.services my-mysql,my-rabbit,my-redis
+cf set-env womack-dataflow-server spring.cloud.deployer.cloudfoundry.services my-mysql,my-rabbitmq,my-redis
 cf set-env womack-dataflow-server spring.cloud.deployer.cloudfoundry.username 
 cf set-env womack-dataflow-server spring.cloud.deployer.cloudfoundry.password 
 cf set-env womack-dataflow-server spring.cloud.deployer.cloudfoundry.skip.ssl.validation true
--- submit.sh
+++ submit.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-for i in `seq 100 1000`; do
+for i in `seq 5000 15199`; do
     ID=`echo $RANDOM`
-    STATUS=`gshuf -n1  /tmp/words`
-    PAYLOAD="{ 'authrorizationId': '"${ID}"', 'status': '"${STATUS}"' }"
-    echo $PAYLOAD
-#    curl -i -k -X POST -H "Content-Type:application/json" -d '{  "autorizationId" : "123456",  "status": $STATUS  }' http://localhost:9001
+    STATUS=`gshuf -n1  /tmp/stati`
+    PAYLOAD1='{ "authrorizationId": "'
+    PAYLOAD2="${ID}"
+    PAYLOAD3='", "status": "'
+    PAYLOAD4="${STATUS}"
+    PAYLOAD5='" }'
+    PAYLOAD="${PAYLOAD1}${PAYLOAD2}${PAYLOAD3}${PAYLOAD4}${PAYLOAD5}"
+    echo "${PAYLOAD}"
+    curl -i -k -X POST -H "Content-Type:application/json" -d "${PAYLOAD}" http://localhost:9001
 done
