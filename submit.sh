f16811767e3eb77c24c21579ddab8b7417834665
Steve Womack <stwomack@gmail.com>
2016-09-20 18:53:31 -0500

    yolo

--- submit.sh
+++ submit.sh
@@ -2,19 +2,19 @@
 #set -e -x
 
 LOCAL_URL="http://localhost:9001/"
-URL="https://dataflow-forgettable-undershrub-http-in-http.cfapps.pez.pivotal.io/"
+URL="https://dataflow-spiciest-dhurrie-http-in-http.cfapps.pez.pivotal.io/"
 
-ab -r -l -c 20 -n 1600  -p approved.json -v 0 -T 'application/json' ${URL} &
-ab -r -l -c 20 -n 200  -p declined.json -v 0 -T 'application/json' ${URL} &
-ab -r -l -c 20 -n 100  -p error.json -v 0 -T 'application/json' ${URL} &
-ab -r -l -c 20 -n 10  -p pending.json -v 0 -T 'application/json' ${URL} &
+#ab -r -l -c 20 -n 1600  -p approved.json -v 0 -T 'application/json' ${URL} &
+#ab -r -l -c 20 -n 200  -p declined.json -v 0 -T 'application/json' ${URL} &
+#ab -r -l -c 20 -n 100  -p error.json -v 0 -T 'application/json' ${URL} &
+#ab -r -l -c 20 -n 10  -p pending.json -v 0 -T 'application/json' ${URL} &
+#
+#ab -r -l -c 20 -n 600  -p approved.json -v 0 -T 'application/json' ${URL} &
+#ab -r -l -c 20 -n 200  -p declined.json -v 0 -T 'application/json' ${URL}  &
+#ab -r -l -c 20 -n 100  -p error.json -v 0 -T 'application/json' ${URL} &
+#ab -r -l -c 20 -n 10  -p pending.json -v 0 -T 'application/json' ${URL} &
 
-ab -r -l -c 20 -n 600  -p approved.json -v 0 -T 'application/json' ${URL} &
-ab -r -l -c 20 -n 200  -p declined.json -v 0 -T 'application/json' ${URL}  &
-ab -r -l -c 20 -n 100  -p error.json -v 0 -T 'application/json' ${URL} &
-ab -r -l -c 20 -n 100  -p pending.json -v 0 -T 'application/json' ${URL} &
-
-for i in `seq 1 50`; do
+for i in `seq 1 500`; do
     ID=`echo $RANDOM`
     STATUS=`gshuf -n1  stati`
     PAYLOAD1='{ "authrorizationId": "'
